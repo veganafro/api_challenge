c70f0380c4adf747bf184b9bfbabc715f52385ae
Jeremy Muhia <jmuhia80@gmail.com>
2016-08-30 18:54:18 -0400

    update python script

--- api_test.py
+++ api_test.py
@@ -1,10 +1,14 @@
-import urllib2
+import requests
 import json
 
 token = "7629edd01fdbb13225e5ffed34449294"
-github
+github = "https://github.com/veganafro/api_challenge"
 end_point = "http://challenge.code2040.org/api/register"
 
-json_obj = urllib2.urlopen(end_point)
+data = {'token': token,
+        'github': github}
 
-json.load(json_obj)
+with open('result.json', 'w') as fp:
+    json_data = json.dump(data, fp, indent=4, sort_keys=False)
+
+ret = requests.post(end_point, json=json_data)
