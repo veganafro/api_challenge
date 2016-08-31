a80b29fc675d52834875feaacdeb133eddfb0e25
Jeremy Muhia <jmuhia80@gmail.com>
2016-08-30 20:40:13 -0400

    complete API call challenge

--- api_test.py
+++ api_test.py
@@ -8,7 +8,14 @@ end_point = "http://challenge.code2040.org/api/register"
 data = {'token': token,
         'github': github}
 
-with open('result.json', 'w') as fp:
-    json_data = json.dump(data, fp, indent=4, sort_keys=False)
+with open('results.json', 'w') as empty_json_file:
+    json.dump(data, empty_json_file, indent=4)
+    empty_json_file.close()
+
+with open('results.json', 'rb') as full_json_file:
+    json_data = json.load(full_json_file)
+    full_json_file.close()
 
 ret = requests.post(end_point, json=json_data)
+
+print(ret.status_code)
